ba74304173bb73bf40ebaf05481876716b1dcbe2
0lejk4 <odubinskiy@ukr.net>
2022-06-09 11:02:46 +0300

    Refactor macroses

--- modules/core/src/main/scala-3/validation/syntax/FieldSyntax.scala
+++ modules/core/src/main/scala-3/validation/syntax/FieldSyntax.scala
@@ -15,7 +15,7 @@ trait FieldSyntax {
       * val res1: jap.fields.Field[String] = request.user.name:ann
       * }}}
       */
-    inline def sub[S](inline selector: P => S): Field[S] = ${ FieldMacro.subImpl('field, 'selector) }
+    inline def sub[S](inline selector: P => S): Field[S] = ${ FieldMacro.subImpl[P, S]('field, 'selector) }
   }
 
   extension (field: Field.type) {
@@ -30,7 +30,9 @@ trait FieldSyntax {
       * val field: jap.fields.Field[String] = request.user.name:ann
       * }}}
       */
-    inline def from[V](inline value: V): Field[V] = ${ FieldMacro.fromImpl('value, includeIdent = true) }
+    inline def from[V](inline value: V): Field[V] = ${
+      FieldMacro.fromImpl[V]('value, includeIdent = true, title = "Field.from")
+    }
 
     /** Similar to [[Field.from]] but drops first selector path
       *
@@ -42,6 +44,8 @@ trait FieldSyntax {
       * val field: jap.fields.Field[String] = user.name:ann
       * }}}
       */
-    inline def sub[V](inline value: V): Field[V] = ${ FieldMacro.fromImpl('value, includeIdent = false) }
+    inline def sub[V](inline value: V): Field[V] = ${
+      FieldMacro.fromImpl[V]('value, includeIdent = false, title = "Field.sub")
+    }
   }
 }
